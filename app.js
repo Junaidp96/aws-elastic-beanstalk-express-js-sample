dd4035d0b2e149f68101ebc3c30c990342514eb5
Junaid Pinjari A <166369646+Junaidp96@users.noreply.github.com>
2024-04-23 14:10:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('lets check if it work from github instead of git'));
+app.get('/', (req, res) => res.send('changes made for review'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
