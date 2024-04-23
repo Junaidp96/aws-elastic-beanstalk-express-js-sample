b624a08dad9b609c34767e084d4f6e55deebedde
mlops <pinjarijunaid@gmail.com>
2024-04-23 13:14:44 +0530

    1 st change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! junaid'));
+app.get('/', (req, res) => res.send('i dont know but it works somehow'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
