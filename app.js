91347d546c568637a762e362b93d61d56c10e651
mlops <pinjarijunaid@gmail.com>
2024-05-01 18:10:31 +0530

    making changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('i dont know but it works somehow'));
+app.get('/', (req, res) => res.send('i dont know but it works '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
